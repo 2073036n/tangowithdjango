3b4abc182fb1a8e95ef19c20f0d5fc412f496cc2
unknown <2073036n@BO715-2-02.ad.dcs.gla.ac.uk>
2015-01-20 10:18:38 +0000

    Chapter 4 updated to include about page

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse('Rango says hey there world! <html><a href=about/>about</a></html>')
+
+def about(request):
+    return HttpResponse('This tutorial has been put together by Timothy Ness, 2073036n. <html><a href=http://127.0.0.1:8000/rango/>index</a></html>')
+
