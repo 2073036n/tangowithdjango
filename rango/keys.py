5059dbeb64be8f4fbb0dcee9f4c28e46b5e6d1ae
unknown <2073036n@BO715-7-05.ad.dcs.gla.ac.uk>
2015-03-05 15:58:06 +0000

    Chapter 15 - completed

---
+++ rango/keys.py
@@ -0,0 +1,1 @@
+BING_API_KEY = 'c3rIh07tyZ4a7v1nkApKlpG3YBECtDvoDKyVtL95QCw'
--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
 from django.contrib.auth.models import User
+from rango.bing_search import run_query
 
 def index(request):
     
@@ -261,3 +262,15 @@ def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
 
 
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
